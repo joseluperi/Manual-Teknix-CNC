3e3dceab22ba3fb16d2c207b446bce8265d7a939
joseluperi <59607426+joseluperi@users.noreply.github.com>
2022-11-08 13:36:57 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Manual de Ususario Teknix CNC'
+copyright = '2022, Teknix Arg.'
+author = 'Teknix Arg'
 
 release = '0.1'
 version = '0.1.0'
