fad16765c85817bcc368af54112ea15984b802b2
joseluperi <59607426+joseluperi@users.noreply.github.com>
2025-08-07 17:44:31 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,12 +20,14 @@ extensions = [
 ]
 
 intersphinx_mapping = {
+    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
     'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
 intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
+
 
 # -- Options for HTML output
 
